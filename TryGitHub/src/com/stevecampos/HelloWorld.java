32e59f7c18e3c73a7c1c7894b8662db83055b3bb
Steve <js7campos@gmail.com>
2014-04-20 16:55:08 -0500

    Second implementation.

--- TryGitHub/src/com/stevecampos/HelloWorld.java
+++ TryGitHub/src/com/stevecampos/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World, I try Egit(Git) with eclipse");
+		System.out.println("I am tired to try more, See you tomorrow Eclipse and Egit. Bye");
 	}
 
 }
