81ebb4b93cad03539bb0b2a17ffbb5a65dea3b16
Steve <js7campos@gmail.com>
2014-04-20 16:52:09 -0500

    First implementation, Imprime en pantalla un Hola mundo.

---
+++ TryGitHub/src/com/stevecampos/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.stevecampos;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World, I try Egit(Git) with eclipse");
+	}
+
+}
